7b0af3ac7e0180dc4f37f38eefd5fd96d3a2b5b3
Repher Akello <akellorepher@gmail.com>
2024-11-09 16:31:08 +0300

    adding the django project trial

--- name/name/settings.py
+++ name/name/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gas_managment.apps.gasmanagmentConfig',
 ]
 
 MIDDLEWARE = [
